79eac318b75a4e49d796dfdf069adafb283c5865
basemomar94 <basemomar2012@gmail.com>
2021-10-05 22:51:18 +0200

    v

--- app/src/main/java/com/example/gym/About.java
+++ app/src/main/java/com/example/gym/About.java
@@ -6,13 +6,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class About extends AppCompatActivity {
-    ActionBar actionBar = getSupportActionBar();
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+        ActionBar actionBar = getSupportActionBar();
+
+
         actionBar.setDisplayHomeAsUpEnabled(true);
     }
 }
