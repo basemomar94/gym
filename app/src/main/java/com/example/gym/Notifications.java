e22bf2dfcbac00d00a6bbb832d7158d07c02eaea
basemomar94 <basemomar2012@gmail.com>
2021-10-08 18:41:02 +0200

    v

--- app/src/main/java/com/example/gym/Notifications.java
+++ app/src/main/java/com/example/gym/Notifications.java
@@ -2,11 +2,19 @@ package com.example.gym;
 
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class Notifications extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private NotificationAdapter notificationAdapter;
+    private ArrayList<Notificationitem> notificationitems;
 
 
     @Override
@@ -16,9 +24,21 @@ public class Notifications extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setTitle("Notifications");
+        recyclerView = findViewById(R.id.recycleview);
+        notificationitems = new ArrayList<>();
+        for (int i = 0; i < 10; i++) {
+            notificationitems.add(new Notificationitem("new offer dont waste it"));
+            notificationitems.add(new Notificationitem("A big discount is waiting you"));
+            notificationitems.add(new Notificationitem("check out our new suana"));
+            notificationitems.add(new Notificationitem("we have changed the pool schedule"));
+
+
+        }
+
+        notificationAdapter = new NotificationAdapter(this, notificationitems);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(notificationAdapter);
     }
-
-
 
 
 }
